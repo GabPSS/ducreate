86125ae56e29a9d128a6ba45b232338028cf21ec
Gabriel P <gabpilotto.gps@gmail.com>
2022-06-30 11:28:23 -0300

    Added imgfimptest and developed libimgfetch

---
+++ testing/imgfimptest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using libimgfetch;
+
+namespace imgfimptest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Stream> Files = ImgFetch.GetImageStreams(Services.google,"dog");
+            for (int i = 0;i<Files.Count;i++)
+            {
+                try
+                {
+                    FileStream filewrite = File.OpenWrite(i.ToString("000"));
+                    Files[i].CopyTo(filewrite);
+                    filewrite.Close();
+                }
+                catch (Exception x)
+                {
+                    Console.Error.WriteLine("(Writing error) " + x.Message);
+                }
+            }
+        }
+    }
+}
