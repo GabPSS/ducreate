f506d1dde4c3dd7b62bcdadf1b39e61294e2debf
Gabriel P <gabpilotto.gps@gmail.com>
2022-07-03 15:39:25 -0300

    Changes to guiiftestdotnet

--- testing/guiiftest/GuiIfTestDotnet/Form1.Designer.cs
+++ testing/guiiftest/GuiIfTestDotnet/Form1.Designer.cs
@@ -32,29 +32,36 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 41);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(14, 47);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(656, 363);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(765, 419);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(14, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(478, 20);
+            this.textBox1.Size = new System.Drawing.Size(557, 23);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(593, 12);
+            this.button1.Location = new System.Drawing.Point(692, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(88, 27);
             this.button1.TabIndex = 2;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
@@ -67,26 +74,53 @@
             this.comboBox1.Items.AddRange(new object[] {
             "google",
             "pixabay"});
-            this.comboBox1.Location = new System.Drawing.Point(496, 12);
+            this.comboBox1.Location = new System.Drawing.Point(579, 14);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(91, 21);
+            this.comboBox1.Size = new System.Drawing.Size(106, 23);
             this.comboBox1.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 481);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(793, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(185, 17);
+            this.toolStripStatusLabel1.Text = "Faça uma pesquisa para começar!";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 416);
+            this.ClientSize = new System.Drawing.Size(793, 503);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "libimgfetch Demo";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +132,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripProgressBar toolStripProgressBar1;
     }
 }
--- testing/guiiftest/GuiIfTestDotnet/Form1.cs
+++ testing/guiiftest/GuiIfTestDotnet/Form1.cs
@@ -11,24 +11,69 @@ namespace GuiIfTestDotnet
 
         private void button1_Click(object sender, EventArgs e)
         {
-            List<Stream> fileStreams = ImgFetch.GetImageStreams((Services)comboBox1.SelectedIndex, textBox1.Text);
-            foreach (Stream file in fileStreams)
+            List<Stream> fileStreams = new List<Stream>();
+            Services service = (Services)comboBox1.SelectedIndex;
+            string query = textBox1.Text;
+            foreach (Control c in flowLayoutPanel1.Controls)
             {
-                try
-                {
-                    Image img = Bitmap.FromStream(file);
-                    PictureBox pbx = new PictureBox();
-                    pbx.Width = 322;
-                    pbx.Height = 181;
-                    pbx.SizeMode = PictureBoxSizeMode.Zoom;
-                    pbx.Image = img;
-                    flowLayoutPanel1.Controls.Add(pbx);
-                }
-                catch (Exception x)
-                {
-                    Console.Error.WriteLine("Error while creating image from streams: " + x.Message);
-                }
+                c.Dispose(); 
             }
+            flowLayoutPanel1.Controls.Clear();
+
+            Cursor = Cursors.WaitCursor;
+            Thread imgfetch = new Thread(() => { 
+                WriteLine("Obtendo imagens através da internet...");
+                SetProgress(20,ProgressBarStyle.Marquee);
+                fileStreams = ImgFetch.GetImageStreams(service,query);
+                
+                int i = 0;
+                foreach (Stream file in fileStreams)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        try
+                        {
+                            Image img = Bitmap.FromStream(file);
+                            PictureBox pbx = new PictureBox();
+                            pbx.Width = 322;
+                            pbx.Height = 181;
+                            pbx.SizeMode = PictureBoxSizeMode.Zoom;
+                            pbx.Image = img;
+                            flowLayoutPanel1.Controls.Add(pbx);
+                            i++;
+                        }
+                        catch (Exception x)
+                        {
+                            Console.Error.WriteLine("Error while creating image from streams: " + x.Message);
+                        }
+                    }));
+                }
+                WriteLine(i + " imagens adicionadas.");
+                this.Invoke(new Action(() =>
+                {
+                    Cursor = Cursors.Default;
+                }));
+                SetProgress(100, ProgressBarStyle.Continuous);
+            });
+            imgfetch.Name = "ImgFetch";
+            imgfetch.Start();
+        }
+
+        void WriteLine(string text)
+        {
+            this.Invoke(new Action(() =>
+            {
+                statusStrip1.Items[0].Text = text;
+            }));
+        }
+
+        void SetProgress(int value, ProgressBarStyle style)
+        {
+            this.Invoke(new Action(() =>
+            {
+                toolStripProgressBar1.Value = value;
+                toolStripProgressBar1.Style = style;
+            }));
         }
     }
 }
