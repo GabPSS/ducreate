ebd14f53d3308a6911db6efc7f1ca8b8c0ed620f
Gabriel P <gabpilotto.gps@gmail.com>
2022-08-08 15:03:52 -0300

    (langapi) Added working code

--- testing/langprocessing/langapi/Program.cs
+++ testing/langprocessing/langapi/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace LangAPI
+﻿using System.IO;
+
+namespace LangAPI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string filePath = args[0];
+            string[] document = File.ReadAllLines(filePath);
+
+            API api = new API(document);
+            api.GetKeywords();
             
+            if (api.Keywords is not null)
+            {
+                Console.WriteLine("Keywords:");
+                foreach (string keyword in api.Keywords)
+                {
+                    Console.WriteLine(keyword);
+                }
+            }
+            else
+            {
+                Console.WriteLine("api.Keywords was null");
+            }
         }
     }
 }
