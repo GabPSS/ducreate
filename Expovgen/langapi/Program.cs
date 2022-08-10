3e43135ff7c97006dc1546c099a59cd965a3ecac
Gabriel P <gabpilotto.gps@gmail.com>
2022-08-10 18:28:29 -0300

    Reorganizando arquivos

---
+++ Expovgen/langapi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace LangAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = args[0];
+            string[] document = File.ReadAllLines(filePath);
+
+            API api = new API(document);
+            api.GetKeywords();
+            
+            if (api.Keywords is not null)
+            {
+                Console.WriteLine("Keywords:");
+                foreach (string keyword in api.Keywords)
+                {
+                    Console.WriteLine(keyword);
+                }
+            }
+            else
+            {
+                Console.WriteLine("api.Keywords was null");
+            }
+        }
+    }
+}
