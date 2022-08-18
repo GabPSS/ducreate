f0ccf5e7579be4651659ca3cb8eed6ba164eb5bf
Gabriel P <gabpilotto.gps@gmail.com>
2022-08-18 00:49:10 -0300

    Updated files

--- TestingProjects/ffmstitchtest/stitchtest.py
+++ TestingProjects/ffmstitchtest/stitchtest.py
@@ -1,4 +1,7 @@
-import ffmpeg
+import ffmpeg from ffmpeg-python
+import os
+
+print(os.getcwd())
 
 in_file = ffmpeg.input("input.mp4")
 
@@ -8,7 +11,7 @@ in_mus = ffmpeg.input("aud.mp3")
 a1 = in_mus.audio
 
 in_file2 = in_file.drawbox(50,50,120,120, color='Blue', thickness='5')
-in_file2 = in_file2.drawtext('Clipe maravilhoso 1',x='(w-tw)/2',y=986, fontsize='98',box=1,boxcolor='black',fontcolor='white')
+in_file2 = in_file2.drawtext('Este e um video gerado em python!',x='(w-tw)/2',y=986, fontsize='98',box=1,boxcolor='black',fontcolor='white')
 
 conc = ffmpeg.concat(in_file2, a1, v=1, a=1)
 out = ffmpeg.output(conc, 'movie.mp4')
