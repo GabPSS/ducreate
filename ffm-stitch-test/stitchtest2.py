caf27e702070023d6239f3a5c5d79286e13a9ac7
GabPSS <45743069+GabPSS@users.noreply.github.com>
2022-08-22 15:54:06 -0300

    Delete ffm-stitch-test directory

--- ffm-stitch-test/stitchtest2.py
+++
@@ -1,8 +0,0 @@
-from moviepy.editor import *
-
-clip = VideoFileClip('input.mp4')
-txtclp = TextClip('Testing out moviepy', fontsize=70, color='blue')
-txtclp = txtclp.set_position('center').set_duration(3)
-
-video = CompositeVideoClip([clip, txtclp])
-clip.write_videofile('moviepy.mp4')
