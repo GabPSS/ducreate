92755bbf451a08edcb3b763df9210a6ae2721359
Gabriel P <gabpilotto.gps@gmail.com>
2022-09-26 20:36:36 -0300

    Updated stitch test

--- ffm-stitch-test/stitchtest2.py
+++ ffm-stitch-test/stitchtest2.py
@@ -4,36 +4,80 @@ from moviepy.editor import *
 vid_height = 1366
 vid_width = 768
 
-img_clip1 = ImageClip("002.png", duration=2)
-img_clip1 = resize.resize(img_clip1,width=vid_width,height=vid_height)
+# intializes all clips
 
-#img_clip1 = img_clip1.resize(1920,1080)
-img_clip2 = ImageClip("003.png",duration=2)
-img_clip2 = resize.resize(img_clip2,width=vid_width,height=vid_height)
+img1 = ImageClip("007.png",duration=5)
+img2 = ImageClip("004.png",duration=5)
+img3 = ImageClip("005.png",duration=5)
+img4 = ImageClip("008.png",duration=5)
+aud = AudioFileClip("aud.mp3")
+aud = aud.subclip(0,20)
+
+# initializes text clips
+
+txt1 = TextClip("Primeira legenda", fontsize=70, color='white',bg_color='black')
+txt1 = txt1.set_position('bottom')
+txt1 = txt1.set_duration(7)
+txt2 = TextClip("Segunda legenda", fontsize=70, color='white',bg_color='black')
+txt2 = txt2.set_position('bottom')
+txt2 = txt2.set_duration(4)
+txt3 = TextClip("Terceira legenda", fontsize=70, color='white',bg_color='black')
+txt3 = txt3.set_position('bottom')
+txt3 = txt3.set_duration(4)
+txt4 = TextClip("Quarta legenda", fontsize=70, color='white',bg_color='black')
+txt4 = txt4.set_position('bottom')
+txt4 = txt4.set_duration(5)
+
+# generates video divisions
+
+video = concatenate_videoclips([img1,img2,img3,img4])
+videodiv1 = video.subclip(0,7)
+videodiv2 = video.subclip(7,11)
+videodiv3 = video.subclip(11,15)
+videodiv4 = video.subclip(15,20)
+
+# adds captions
+
+videodiv1 = CompositeVideoClip([videodiv1,txt1])
+videodiv2 = CompositeVideoClip([videodiv2,txt2])
+videodiv3 = CompositeVideoClip([videodiv3,txt3])
+videodiv4 = CompositeVideoClip([videodiv4,txt4])
+
+# brings everything together with audio
+finalvid = concatenate_videoclips([videodiv1,videodiv2,videodiv3,videodiv4])
+finalvid = finalvid.set_audio(aud)
+finalvid.write_videofile('concat.mp4',fps=30)
+
+# img_clip1 = ImageClip("002.png", duration=2)
+# img_clip1 = resize.resize(img_clip1,width=vid_width,height=vid_height)
+
+# #img_clip1 = img_clip1.resize(1920,1080)
+# img_clip2 = ImageClip("003.png",duration=2)
+# img_clip2 = resize.resize(img_clip2,width=vid_width,height=vid_height)
 
 
-aud_bg = AudioFileClip("aud.mp3")
-aud_bg = aud_bg.volumex(0.2)
-aud_bg = aud_bg.subclip(0,4)
+# aud_bg = AudioFileClip("aud.mp3")
+# aud_bg = aud_bg.volumex(0.2)
+# aud_bg = aud_bg.subclip(0,4)
 
-aud_speech = AudioFileClip("speech.mp3")
-aud_speech = aud_speech.subclip(0,4)
+# aud_speech = AudioFileClip("speech.mp3")
+# aud_speech = aud_speech.subclip(0,4)
 
 
-txt_clip1 = TextClip("This one was made by MoviePy!!", fontsize=70, color='white',bg_color='black')
-txt_clip1 = txt_clip1.set_position('bottom')
-txt_clip1 = txt_clip1.set_duration(2)
+# txt_clip1 = TextClip("This one was made by MoviePy!!", fontsize=70, color='white',bg_color='black')
+# txt_clip1 = txt_clip1.set_position('bottom')
+# txt_clip1 = txt_clip1.set_duration(2)
 
-txt_clip2 = TextClip("This is a second text!!", fontsize=70, color='white', bg_color='black')
-txt_clip2 = txt_clip2.set_position('bottom')
-txt_clip2 = txt_clip2.set_duration(2)
+# txt_clip2 = TextClip("This is a second text!!", fontsize=70, color='white', bg_color='black')
+# txt_clip2 = txt_clip2.set_position('bottom')
+# txt_clip2 = txt_clip2.set_duration(2)
 
-clip1 = CompositeVideoClip([img_clip1,txt_clip1])
-clip2 = CompositeVideoClip([img_clip2,txt_clip2])
-clip_aud = CompositeAudioClip([aud_bg,aud_speech])
+# clip1 = CompositeVideoClip([img_clip1,txt_clip1])
+# clip2 = CompositeVideoClip([img_clip2,txt_clip2])
+# clip_aud = CompositeAudioClip([aud_bg,aud_speech])
 
-AnArray = [clip1,clip2]
+# AnArray = [clip1,clip2]
 
-video = concatenate_videoclips(AnArray)
-video = video.set_audio(clip_aud)
-video.write_videofile('moviepy.mp4', fps=30)
+# video = concatenate_videoclips(AnArray)
+# video = video.set_audio(clip_aud)
+# video.write_videofile('moviepy.mp4', fps=30)
