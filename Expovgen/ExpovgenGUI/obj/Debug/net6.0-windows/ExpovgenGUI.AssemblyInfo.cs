dad138a88cb5b04c7609f57cdfcc3d8cd3a61ff0
Gabriel P <gabpilotto.gps@gmail.com>
2022-10-21 19:57:08 -0300

    Updated files

--- Expovgen/ExpovgenGUI/obj/Debug/net6.0-windows/ExpovgenGUI.AssemblyInfo.cs
+++ Expovgen/ExpovgenGUI/obj/Debug/net6.0-windows/ExpovgenGUI.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpovgenGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpovgenGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpovgenGUI")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.1.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
