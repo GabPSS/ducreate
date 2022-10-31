439d8219664577772817ed9fae1efb2ccc59cc1b
GabPSS <45743069+GabPSS@users.noreply.github.com>
2022-10-31 19:09:10 -0300

    Delete Expovgen/Audioworks directory

--- Expovgen/Audioworks/perform_tts.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-
-def PerformTTS(file_path: str, output_path: str):
-    f = open(file_path,mode="r",encoding="utf-8")
-    text = f.read()
-    f.close()
-    i = 0
-    tts = gTTS(text, lang="pt")
-    tts.save(output_path)
